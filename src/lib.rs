4683d97a70326b7e6c7fabd29dad151eb6c5f33e
Kat Marchán <kzm@zkat.tech>
2019-11-10 22:01:43 -0800

    wip: I think this is all I can do before http-cache-semantics exists

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,13 @@ use async_trait::async_trait;
 use futures::future::BoxFuture;
 use http::HeaderMap;
 use httpdate;
-use surf::middleware::{HttpClient, Body, Middleware, Next, Request, Response};
+use surf::middleware::{Body, HttpClient, Middleware, Next, Request, Response};
 
 #[async_trait]
 pub trait CacheManager {
     async fn get(&self, req: &Request) -> Result<Option<Response>, surf::Exception>;
     async fn put(&self, req: &Request, res: Response) -> Result<Response, surf::Exception>;
+    async fn delete(&self, req: &Request) -> Result<(), surf::Exception>;
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -46,7 +47,7 @@ impl<T: CacheManager> Cache<T> {
         }
 
         if let Some(mut res) = self.cache_manager.get(&req).await? {
-            if let Some(warning_code) = self.get_warning_code(&res) {
+            if let Some(warning_code) = get_warning_code(&res) {
                 // https://tools.ietf.org/html/rfc7234#section-4.3.4
                 //
                 // If a stored response is selected for update, the cache MUST:
@@ -62,7 +63,7 @@ impl<T: CacheManager> Cache<T> {
                 }
             }
 
-            if self.mode == CacheMode::Default && !self.is_stale(&req, &res) {
+            if self.mode == CacheMode::Default && !is_stale(&req, &res) {
                 Ok(res)
             } else if self.mode == CacheMode::Default {
                 Ok(self.conditional_fetch(req, res, client, next).await?)
@@ -71,7 +72,7 @@ impl<T: CacheManager> Cache<T> {
                 // SHOULD be included if the cache is intentionally disconnected from
                 // the rest of the network for a period of time.
                 // (https://tools.ietf.org/html/rfc2616#section-14.46)
-                self.add_warning(&req.uri(), res.headers_mut(), 112, "Disconnected operation");
+                add_warning(&req.uri(), res.headers_mut(), 112, "Disconnected operation");
                 Ok(res)
             } else {
                 Ok(self.remote_fetch(req, client, next).await?)
@@ -84,46 +85,6 @@ impl<T: CacheManager> Cache<T> {
         }
     }
 
-    fn get_warning_code(&self, res: &Response) -> Option<usize> {
-        res.headers().get("Warning").and_then(|hdr| {
-            hdr.to_str()
-                .ok()
-                .and_then(|s| s.chars().take(3).collect::<String>().parse().ok())
-        })
-    }
-
-    fn is_stale(&self, req: &Request, res: &Response) -> bool {
-        // TODO - most of what this looks like is gonna depend on http-cache-semantics
-        unimplemented!()
-    }
-
-    fn add_warning(&self, uri: &http::Uri, headers: &mut HeaderMap, code: usize, message: &str) {
-        //   Warning    = "Warning" ":" 1#warning-value
-        // warning-value = warn-code SP warn-agent SP warn-text [SP warn-date]
-        // warn-code  = 3DIGIT
-        // warn-agent = ( host [ ":" port ] ) | pseudonym
-        //                 ; the name or pseudonym of the server adding
-        //                 ; the Warning header, for use in debugging
-        // warn-text  = quoted-string
-        // warn-date  = <"> HTTP-date <">
-        // (https://tools.ietf.org/html/rfc2616#section-14.46)
-        //
-        headers.append(
-            "Warning",
-            http::HeaderValue::from_str(
-                format!(
-                    "{} {} {:?} \"{}\"",
-                    uri.host().expect("Invalid URL"),
-                    code,
-                    message,
-                    httpdate::fmt_http_date(SystemTime::now())
-                )
-                .as_str(),
-            )
-            .expect("Failed to generate warning string"),
-        );
-    }
-
     async fn conditional_fetch<'a, C: HttpClient>(
         &self,
         mut req: Request,
@@ -131,43 +92,40 @@ impl<T: CacheManager> Cache<T> {
         client: C,
         next: Next<'a, C>,
     ) -> Result<Response, surf::Exception> {
-        self.set_revalidation_headers(&mut req);
-        let uri = req.uri().clone();
-        let req_headers = req.headers().clone();
+        set_revalidation_headers(&mut req);
+        let copied_req = clone_req(&req);
         match self.remote_fetch(req, client, next).await {
             Ok(cond_res) => {
-                if cond_res.status().is_server_error() && self.must_revalidate(&cached_res) {
+                if cond_res.status().is_server_error() && must_revalidate(&cached_res) {
                     //   111 Revalidation failed
                     //   MUST be included if a cache returns a stale response
                     //   because an attempt to revalidate the response failed,
                     //   due to an inability to reach the server.
                     // (https://tools.ietf.org/html/rfc2616#section-14.46)
-                    self.add_warning(&uri, cached_res.headers_mut(), 111, "Revalidation failed");
+                    add_warning(
+                        &copied_req.uri(),
+                        cached_res.headers_mut(),
+                        111,
+                        "Revalidation failed",
+                    );
                     Ok(cached_res)
                 } else if cond_res.status() == http::StatusCode::NOT_MODIFIED {
-                    // TODO - simplify/extract into a function? This is super verbose.
                     let mut res = http::Response::builder();
                     res.status(cond_res.status());
                     let headers = res.headers_mut().expect("Couldn't get headers.");
                     for (key, value) in cond_res.headers().into_iter() {
                         headers.append(key, value.clone());
                     }
-                    let res = res.body(cached_res.into_body()).unwrap();
-                    let mut dummy_req = http::Request::builder();
-                    dummy_req.uri(uri);
-                    for (key, value) in req_headers.into_iter() {
-                        dummy_req.header(key.unwrap(), value.clone());
-                    }
                     // TODO - set headers to revalidated response headers? Needs http-cache-semantics.
-                    let dummy_req = dummy_req.body(Body::empty()).unwrap();
-                    let res = self.cache_manager.put(&dummy_req, res).await?;
+                    let res = res.body(cached_res.into_body()).unwrap();
+                    let res = self.cache_manager.put(&copied_req, res).await?;
                     Ok(res)
                 } else {
                     Ok(cached_res)
                 }
             }
             Err(e) => {
-                if self.must_revalidate(&cached_res) {
+                if must_revalidate(&cached_res) {
                     Err(e)
                 } else {
                     let mut headers = cached_res.headers_mut();
@@ -176,35 +134,23 @@ impl<T: CacheManager> Cache<T> {
                     //   because an attempt to revalidate the response failed,
                     //   due to an inability to reach the server.
                     // (https://tools.ietf.org/html/rfc2616#section-14.46)
-                    self.add_warning(&uri, &mut headers, 111, "Revalidation failed");
+                    add_warning(&copied_req.uri(), &mut headers, 111, "Revalidation failed");
                     //   199 Miscellaneous warning
                     //   The warning text MAY include arbitrary information to
                     //   be presented to a human user, or logged. A system
                     //   receiving this warning MUST NOT take any automated
                     //   action, besides presenting the warning to the user.
                     // (https://tools.ietf.org/html/rfc2616#section-14.46)
-                    self.add_warning(&uri, &mut headers, 199, format!("Miscellaneous Warning {}", e).as_str());
+                    add_warning(
+                        &copied_req.uri(),
+                        &mut headers,
+                        199,
+                        format!("Miscellaneous Warning {}", e).as_str(),
+                    );
 
                     Ok(cached_res)
                 }
             }
-        }
-    }
-
-    fn set_revalidation_headers(&self, mut req: &Request) {
-        // TODO - need http-cache-semantics to do this.
-        unimplemented!()
-    }
-
-    fn must_revalidate(&self, res: &Response) -> bool {
-        if let Some(val) = res
-            .headers()
-            .get("Cache-Control")
-            .and_then(|h| h.to_str().ok())
-        {
-            val.to_lowercase().contains("must-revalidate")
-        } else {
-            false
         }
     }
 
@@ -214,11 +160,94 @@ impl<T: CacheManager> Cache<T> {
         client: C,
         next: Next<'a, C>,
     ) -> Result<Response, surf::Exception> {
-        // TODO - cache
-        Ok(next.run(req, client).await?)
+        let copied_req = clone_req(&req);
+        let res = next.run(req, client).await?;
+        let is_method_get_head =
+            copied_req.method() == http::Method::GET || copied_req.method() == http::Method::HEAD;
+        let is_cacheable = self.mode != CacheMode::NoStore
+            && is_method_get_head
+            && res.status() == http::StatusCode::OK;
+        // TODO
+        // && policy.is_storable(&req_copy, &res);
+        if is_cacheable {
+            Ok(self.cache_manager.put(&copied_req, res).await?)
+        } else if !is_method_get_head {
+            self.cache_manager.delete(&copied_req).await?;
+            Ok(res)
+        } else {
+            Ok(res)
+        }
     }
 }
 
+fn must_revalidate(res: &Response) -> bool {
+    if let Some(val) = res
+        .headers()
+        .get("Cache-Control")
+        .and_then(|h| h.to_str().ok())
+    {
+        val.to_lowercase().contains("must-revalidate")
+    } else {
+        false
+    }
+}
+
+fn set_revalidation_headers(mut req: &Request) {
+    // TODO - need http-cache-semantics to do this.
+    unimplemented!()
+}
+
+fn get_warning_code(res: &Response) -> Option<usize> {
+    res.headers().get("Warning").and_then(|hdr| {
+        hdr.to_str()
+            .ok()
+            .and_then(|s| s.chars().take(3).collect::<String>().parse().ok())
+    })
+}
+
+fn is_stale(req: &Request, res: &Response) -> bool {
+    // TODO - most of what this looks like is gonna depend on http-cache-semantics
+    unimplemented!()
+}
+
+fn add_warning(uri: &http::Uri, headers: &mut HeaderMap, code: usize, message: &str) {
+    //   Warning    = "Warning" ":" 1#warning-value
+    // warning-value = warn-code SP warn-agent SP warn-text [SP warn-date]
+    // warn-code  = 3DIGIT
+    // warn-agent = ( host [ ":" port ] ) | pseudonym
+    //                 ; the name or pseudonym of the server adding
+    //                 ; the Warning header, for use in debugging
+    // warn-text  = quoted-string
+    // warn-date  = <"> HTTP-date <">
+    // (https://tools.ietf.org/html/rfc2616#section-14.46)
+    //
+    headers.append(
+        "Warning",
+        http::HeaderValue::from_str(
+            format!(
+                "{} {} {:?} \"{}\"",
+                uri.host().expect("Invalid URL"),
+                code,
+                message,
+                httpdate::fmt_http_date(SystemTime::now())
+            )
+            .as_str(),
+        )
+        .expect("Failed to generate warning string"),
+    );
+}
+
+fn clone_req(req: &Request) -> Request {
+    let mut copied_req = http::Request::new(Body::empty());
+    *copied_req.method_mut() = req.method().clone();
+    *copied_req.uri_mut() = req.uri().clone();
+    *copied_req.headers_mut() = req.headers().clone();
+    *copied_req.version_mut() = req.version().clone();
+    copied_req
+}
+
+// TODO - Surf needs to resolve an issue with Body not being Sync
+//        Ref: https://github.com/http-rs/surf/issues/97
 // impl<C: HttpClient, T: CacheManager> Middleware<C> for Cache<T> {
 //     fn handle<'a>(
 //         &'a self,
